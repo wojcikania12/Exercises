00f36b2b54ca695a6e00218dfe66f2fdba04f2d7
wojcikania12 <wojcikania12@wp.pl>
2018-04-05 10:51:08 +0200

    lab3

--- lab4/netstemplateengine/SimpleTemplateEngine.cpp
+++ lab4/netstemplateengine/SimpleTemplateEngine.cpp
@@ -10,24 +10,18 @@ namespace nets {
 
     std::string View::Render(const std::unordered_map<std::string, std::string> &model) const {
         std::string temp = input;
-        int match_index;
+        size_t match_index, regex_index = 0;
         std::regex pattern(R"(\{\{(\w*)\}\})");
         std::smatch found_match;
-        while(regex_search(temp, found_match, pattern)){
-            for(auto i : found_match){
-                match_index = temp.find(i.str());
-                while(match_index != std::string::npos) {
-                    for (auto iterator = model.begin(); iterator != model.end(); ++iterator)
-                        if (iterator->first == i.str()) {
-                            std::string temp_str = i.str();
-                            temp.replace(match_index, temp_str.length(), iterator->second);
-                        }
-                        else{
-                            temp.replace(match_index, i.length(), "");
-                        }
-                }
-            }
-
+        regex_search(temp, found_match, pattern);
+            match_index = temp.find(found_match[regex_index]);
+                for (const auto &iterator : model) {
+                    std::string str = found_match[(regex_index + 1)];
+                    if (iterator.first == str) {
+                        temp.replace(match_index, found_match[regex_index].length(), iterator.second);
+                    } else {
+                        temp.replace(match_index, found_match[regex_index].length(), "");
+                    }
         }
         return temp;
     }
