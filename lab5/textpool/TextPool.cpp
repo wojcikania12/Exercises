352cda9dadbee5d716d4bd324edf522906bbe4d3
wojcikania12 <wojcikania12@wp.pl>
2018-04-12 13:57:57 +0200

    lab3

--- lab5/textpool/TextPool.cpp
+++ lab5/textpool/TextPool.cpp
@@ -32,15 +32,17 @@ namespace pool{
 
     std::experimental::string_view TextPool::Intern(const std::string &str){
         bool found = false;
+        std::string temp;
         std::string i;
         for(auto i : words){
             if(i == str){
                 found = true;
+                temp = i;
                 break;
             }
         }
         if(found){
-            return i;
+            return temp;
         }
         else {
             words.insert(str);
